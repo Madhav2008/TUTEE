3aea56a33800edfe3d4b4b25954ff5a920791803
arora <aroramadhav65@gmail.com>
2022-09-08 23:49:24 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tutee',debugShowCheckedModeBanner: false,
+      title: 'Tutee',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
