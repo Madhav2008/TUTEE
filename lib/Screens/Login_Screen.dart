8296338eeb5f38670183027556030c2ba1e928ae
arora <aroramadhav65@gmail.com>
2022-09-09 23:36:53 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -194,31 +194,31 @@ class _LoginScreenState extends State<LoginScreen> {
                         width: 5,
                       ),
                       Container(
-      padding: EdgeInsets.symmetric(horizontal: 55, vertical: 10),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.redAccent,
-            Color.fromRGBO(226, 55, 68, 1.0),
-            Color.fromRGBO(226, 55, 68, 0.80),
-            Color.fromRGBO(226, 55, 68, 0.60),
-          ],
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: Center(
-        child: Text(
-          buttonText,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 17.3,
-          ),
-        ),
-      ),
-    
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 55, vertical: 10),
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.redAccent,
+                              Color.fromRGBO(226, 55, 68, 1.0),
+                              Color.fromRGBO(226, 55, 68, 0.80),
+                              Color.fromRGBO(226, 55, 68, 0.60),
+                            ],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                        child: Center(
+                          child: Text(
+                            buttonText,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17.3,
+                            ),
+                          ),
+                        ),
                         onPress: () {
                           Navigator.pushReplacement(
                             context,
