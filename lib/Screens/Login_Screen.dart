9c1eb63395641cc2e466545a9192b250f544f361
arora <aroramadhav65@gmail.com>
2022-09-09 23:38:22 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -171,9 +171,9 @@ class _LoginScreenState extends State<LoginScreen> {
                       child: Container(
                         width: MediaQuery.of(context).size.width * 0.5,
                         height: 50,
-                        // child: Buttons(
-                          // buttonText: 'Login',
-                        // ),
+                        child: Buttons(
+                          buttonText: 'Login',
+                        ),
                       ),
                     ),
                   ),
@@ -193,42 +193,17 @@ class _LoginScreenState extends State<LoginScreen> {
                       SizedBox(
                         width: 5,
                       ),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 55,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Colors.redAccent,
-                              Color.fromRGBO(226, 55, 68, 1.0),
-                              Color.fromRGBO(226, 55, 68, 0.80),
-                              Color.fromRGBO(226, 55, 68, 0.60),
-                            ],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                          ),
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17.3,
+                      TextSimpleButton(
+                        title: 'Register',
+                        colors: Colors.redAccent,
+                        onPress: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterScreen(),
                             ),
-                          ),
-                        ),
-                        // onPress: () {
-                        //   Navigator.pushReplacement(
-                        //     context,
-                        //     MaterialPageRoute(
-                        //       builder: (context) => RegisterScreen(),
-                        //     ),
-                        //   );
-                        // },
+                          );
+                        },
                       ),
                     ],
                   ),
