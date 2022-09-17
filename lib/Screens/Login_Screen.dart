66bdb643a2cbe4c48b101fe9d229dc1041395fcd
arora <aroramadhav65@gmail.com>
2022-09-17 23:20:11 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -13,7 +13,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // bool _visible = false;
+  bool _visible = false;
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
@@ -22,250 +22,250 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Future.delayed(const Duration(milliseconds: 100), () {
-    //   setState(() {
-    //     _visible = true;
-    //   });
-    // });
+    Future.delayed(const Duration(milliseconds: 100), () {
+      setState(() {
+        _visible = true;
+      });
+    });
     return Scaffold(
       body: SingleChildScrollView(
-        // child: AnimatedOpacity(
-        // opacity: _visible ? 1.0 : 0.0,
-        // duration: Duration(seconds: 1),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: Colors.purple[300],
-            // image: DecorationImage(
-            //   image: AssetImage(
-            //     '../assets/Login.png',
-            //   ),
-            //   fit: BoxFit.cover,
-            // ),
-          ),
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
           child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
             decoration: BoxDecoration(
-                // color: Colors.black54,
-                ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Image.asset(
-                    "../assets/Logo.png",
-                    width: 100,
-                    height: 100,
-                  ),
-                ),
-                Text(
-                  'School',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 70),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                  child: TextField(
-                    controller: emailController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: Colors.blueAccent,
-                          width: 0.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 0.0,
-                        ),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.mail,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Email',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      fillColor: Colors.white,
-                      focusColor: Colors.white,
+              color: Colors.purple[300],
+              // image: DecorationImage(
+              //   image: AssetImage(
+              //     '../assets/Login.png',
+              //   ),
+              //   fit: BoxFit.cover,
+              // ),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black54,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Image.asset(
+                      "../assets/Logo.png",
+                      width: 100,
+                      height: 100,
                     ),
                   ),
-                ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: const BorderSide(
-                          color: Colors.blueAccent,
-                          width: 0.0,
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 0.0,
-                        ),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.white,
-                      ),
-                      hintText: 'Password',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      fillColor: Colors.white,
-                      focusColor: Colors.white,
+                  Text(
+                    'School',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 40),
+                  SizedBox(height: 70),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      controller: emailController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.blueAccent,
+                            width: 0.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 0.0,
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.mail,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        fillColor: Colors.white,
+                        focusColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                    child: TextField(
+                      obscureText: true,
+                      controller: passwordController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Colors.blueAccent,
+                            width: 0.0,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 0.0,
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                        ),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        fillColor: Colors.white,
+                        focusColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 40),
+                      child: GestureDetector(
+                        child: Text(
+                          'Forget password?',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.blue.shade100,
+                          ),
+                        ),
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => ForgetScreen(),
+                          //   ),
+                          // );
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Center(
                     child: GestureDetector(
-                      child: Text(
-                        'Forget password?',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.blue.shade100,
-                        ),
-                      ),
-                      onTap: () {
-                        // Navigator.push(
+                      onTap: () async {
+                        // authService
+                        //     .signInWithEmailAndPassword(
+                        //         emailController.text, passwordController.text)
+                        //     .then((auth) {
+                        // Navigator.pushReplacement(
                         //   context,
                         //   MaterialPageRoute(
-                        //     builder: (context) => ForgetScreen(),
+                        //     builder: (context) => Navigation(),
                         //   ),
                         // );
+                        Fluttertoast.showToast(
+                          msg: "Login Successfully!!",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                        );
+                        // }).catchError((error) {
+                        //   showDialog(
+                        //       context: context,
+                        //       builder: (con) {
+                        //         return AlertDialog(
+                        //           title: Text("Error"),
+                        //           content: Text(error.toString()),
+                        //         );
+                        //       });
+                        // });
                       },
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Center(
-                  child: GestureDetector(
-                    onTap: () async {
-                      // authService
-                      //     .signInWithEmailAndPassword(
-                      //         emailController.text, passwordController.text)
-                      //     .then((auth) {
-                      // Navigator.pushReplacement(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) => Navigation(),
-                      //   ),
-                      // );
-                      Fluttertoast.showToast(
-                        msg: "Login Successfully!!",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.BOTTOM,
-                      );
-                      // }).catchError((error) {
-                      //   showDialog(
-                      //       context: context,
-                      //       builder: (con) {
-                      //         return AlertDialog(
-                      //           title: Text("Error"),
-                      //           content: Text(error.toString()),
-                      //         );
-                      //       });
-                      // });
-                    },
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      height: 50,
                       child: Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 55,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Colors.blueAccent,
-                              Colors.blue.shade700,
-                              Colors.blue.shade800,
-                              Colors.blue.shade900,
-                            ],
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        height: 50,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 55,
+                            vertical: 10,
                           ),
-                          borderRadius: BorderRadius.circular(40),
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 17.3,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.blueAccent,
+                                Colors.blue.shade700,
+                                Colors.blue.shade800,
+                                Colors.blue.shade900,
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 17.3,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Create new account?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    GestureDetector(
-                      child: Text(
-                        'Register',
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Create new account?',
                         style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.blue.shade100,
+                          color: Colors.white,
+                          fontSize: 15,
                         ),
                       ),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterScreen(),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      GestureDetector(
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.blue.shade100,
                           ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-      // ),
     );
   }
 }
