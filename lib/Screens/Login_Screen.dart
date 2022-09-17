b05670dc23a457d331476236be71b7edf35bac0f
arora <aroramadhav65@gmail.com>
2022-09-17 23:38:17 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -109,7 +109,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.blue,
+                            color: Colors.white70,
                             width: 0.0,
                           ),
                         ),
