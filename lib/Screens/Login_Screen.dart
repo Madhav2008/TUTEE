be1f63d31d3052c23c4aa5b210ba8fb364088924
arora <aroramadhav65@gmail.com>
2022-09-17 23:11:55 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -36,7 +36,7 @@ class _LoginScreenState extends State<LoginScreen> {
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             decoration: BoxDecoration(
-              color: Colors.red,
+              color: Colors.purple[800],
               // image: DecorationImage(
               //   image: AssetImage(
               //     '../assets/Login.png',
