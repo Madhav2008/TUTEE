e8f16e87664197a50136f15357c5879ccd997be4
arora <aroramadhav65@gmail.com>
2022-09-21 23:35:46 +0530

    Update

--- lib/Screens/Forgot_Password_Screen.dart
+++ lib/Screens/Forgot_Password_Screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-class ForgetScreen extends StatefulWidget {
+class ForgotScreen extends StatefulWidget {
   const ForgetScreen({Key? key}) : super(key: key);
 
   @override
