a4c14fa31d3ba6f412b7d04197eca79c9909aa3d
arora <aroramadhav65@gmail.com>
2022-09-21 23:30:48 +0530

    Update

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -52,7 +52,8 @@ class _LoginScreenState extends State<LoginScreen> {
                   Padding(
                     padding: const EdgeInsets.only(top: 0.0),
                     child: Image.asset(
-                      "../assets/LogoS.png",color: Colors.white,
+                      "../assets/LogoS.png",
+                      color: Colors.white,
                       width: 400,
                       height: 400,
                     ),
