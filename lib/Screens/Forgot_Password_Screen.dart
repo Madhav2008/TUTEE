b7cd10ec2afa860b1a69424ab04f2c4a121ed579
arora <aroramadhav65@gmail.com>
2022-09-21 23:42:28 +0530

    Update

--- lib/Screens/Forgot_Password_Screen.dart
+++ lib/Screens/Forgot_Password_Screen.dart
@@ -77,7 +77,7 @@ class _LoginScreenState extends State<ForgotScreen> {
                 ),
                 SizedBox(height: 55),
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
                   child: TextField(
                     controller: emailController,
                     style: TextStyle(color: Colors.white),
