01c049c0b7427e52fef5051444b81edbaf9c53a6
arora <aroramadhav65@gmail.com>
2022-09-22 23:48:24 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -85,7 +85,7 @@ class _HomeScreenState extends State<HomeScreen>
                   titleTextStyle: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
-                    letterSpacing: 20,
+                    letterSpacing: 10,
                   ),
                   centerTitle: true,
                   actions: [
