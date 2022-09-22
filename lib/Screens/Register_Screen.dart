9fc0aa09f7a80f0f9a5e37ba47a97966fd9a499c
arora <aroramadhav65@gmail.com>
2022-09-22 13:08:21 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -322,7 +322,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         textOn: "TUTOR",
                         textOff: "TUTEE",
                         textSize: 17,
-                        colorOn: Colors.deepPurple.shade400,
+                        colorOn: Colors.deepPurple,
                         colorOff: Colors.deepPurple.shade600,
                         iconOn: Icons.bookmark,
                         iconOff: Icons.book,
