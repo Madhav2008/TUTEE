86bbaf9f300b84fdd6b6a96f22af2b36dd66007e
arora <aroramadhav65@gmail.com>
2022-09-22 12:15:30 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -306,7 +306,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 LiteRollingSwitch(
                   value: true,
                   textOn: "TUTOR",
-                  textOff: "TUTEE",colorOn: ,
+                  textOff: "TUTEE",colorOn: Colors.greenAccent,
                 ),
                 SizedBox(height: 20),
                 Padding(
