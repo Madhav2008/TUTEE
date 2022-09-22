6b8a15e81d03b099286ee644f2424ad63550bce0
arora <aroramadhav65@gmail.com>
2022-09-22 13:20:25 +0530

    Update

--- lib/Screens/Splash_Screen.dart
+++ lib/Screens/Splash_Screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tutee_app/Screens/Login_Screen.dart';
 // import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -22,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => LandingPage(),
+            builder: (context) => LoginScreen(),
           ),
         );
       },
