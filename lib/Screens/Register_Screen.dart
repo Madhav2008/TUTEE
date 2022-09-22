22f9ab03a3845f955ec60f36309c8f1a617044ce
arora <aroramadhav65@gmail.com>
2022-09-22 12:05:23 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -364,7 +364,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   child: TextField(
                     controller: bioController,
                     expands: true,
-                    maxLines: null,
+                    maxLines: 1,
                     minLines: null,
                     style: TextStyle(color: Colors.white),
                     decoration: InputDecoration(
