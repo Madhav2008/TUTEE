747921ab55d80480dd44d34fb24bd17d19893a10
arora <aroramadhav65@gmail.com>
2022-09-22 23:47:18 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -81,7 +81,7 @@ class _HomeScreenState extends State<HomeScreen>
                 SliverAppBar(
                   backgroundColor: Colors.deepPurple[600],
                   automaticallyImplyLeading: false,
-                  title: 'TUTEE',
+                  title: Text('TUTEE',
                   titleTextStyle: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
