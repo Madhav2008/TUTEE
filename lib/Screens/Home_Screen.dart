f54111e6de04f5eec9a361241e51692f519c5474
arora <aroramadhav65@gmail.com>
2022-09-22 23:38:43 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -223,7 +223,7 @@ class _HomeScreenState extends State<HomeScreen>
                     // );
                   },
                 )
-              : null,
+              : null,floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         ),
       );
 
