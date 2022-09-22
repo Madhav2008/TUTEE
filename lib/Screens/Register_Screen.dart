0c570188d6b64511b9ab08a829f8576535b34837
arora <aroramadhav65@gmail.com>
2022-09-22 12:19:49 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -310,7 +310,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   colorOn: Colors.greenAccent,
                   colorOff: Colors.redAccent,
                   iconOn: Icons.bookmark,
-                  iconOff: Icons.book,onChanged: (bool state) {
+                  iconOff: Icons.book,
+                  onChanged: (bool state) {
       //Use it to manage the different states
       print('Current State of SWITCH IS: $state');
     },
