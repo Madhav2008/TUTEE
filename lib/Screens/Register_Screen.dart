442d521bee278871a644b6c4560fc4442c5fa046
arora <aroramadhav65@gmail.com>
2022-09-22 12:58:14 +0530

    Update

--- lib/Screens/Register_Screen.dart
+++ lib/Screens/Register_Screen.dart
@@ -310,7 +310,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     Text(
                       'TUTEE',
                       style: TextStyle(
-                        fontSize: 25,
+                        fontSize: 20,
                         color: Colors.white,
                       ),
                     ),
